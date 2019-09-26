e244a4e46f8c8302265357ca2cdcbaea0ab5c241
Joshua Weinstein <jweinstein@splunk.com>
2019-09-26 11:29:13 -0700

    mem work

--- include/pik_mem.h
+++ include/pik_mem.h
@@ -8,6 +8,20 @@ int pik_mem_init_capture(PikMachine* mach, size_t cap);
 int pik_mem_init_inst(PikMachine* mach, size_t cap);
 
 int pik_mem_init_all(PikMachine* mach, size_t cap);
+/**
+ * Releases ownership of the group string.
+ * The string must be freed after it's done being
+ * used.
+ */
+Pik_grchr_t* pik_mem_rel_capt(PikMachine* mach);
+/**
+ * Same as the group capture release, but for instructions
+ */
+Pik_ins_t* pik_mem_rel_inst(PikMachine* mach);
+
+int pik_mem_grow_capture(PikMachine* mach, size_t addon);
+
+int pik_mem_grow_inst(PikMachine* mach, size_t addon);
 
 
 #endif // PIKAXE_PIK_MEM_H
--- src/pik_mem.c
+++ src/pik_mem.c
@@ -40,3 +40,35 @@ int pik_mem_init_all(PikMachine* mach, size_t cap)
 {
     return _pik_mem_init(mach, cap, 1, 1);
 }
+
+Pik_grchr_t* pik_mem_rel_capt(PikMachine* mach)
+{
+    Pik_grchr_t* gotcap = mach->capture;
+    mach->capture = NULL;
+    mach->capture_cap = 0;
+    mach->capture_len = 0;
+    return gotcap;
+}
+
+Pik_ins_t* pik_mem_rel_inst(PikMachine* mach)
+{
+    Pik_ins_t* gotins = mach->instructions;
+    mach->instructions = NULL;
+    mach->instructions_cap = 0;
+    mach->instructions_len = 0;
+    return gotins;  
+}
+
+
+
+int pik_mem_grow_capture(PikMachine* mach, size_t addon)
+{
+    // to do
+    return 1;
+}
+
+int pik_mem_grow_inst(PikMachine* mach, size_t addon)
+{
+    // to do
+    return 1;
+}
